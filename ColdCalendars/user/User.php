5c2fa343f4c87bb514353e742adaeac766e719c3
awashburn awashburn <awashburn1web@hotmail.com>
2014-03-18 16:36:32 -0500

    changed something
    Signed-off-by: thomasaj91 <thomasaj@uwm.edu>

--- ColdCalendars/user/User.php
+++ ColdCalendars/user/User.php
@@ -2,6 +2,19 @@
 include_once (__DIR__ . '/../DB.php');
 class User {
 	private static $getUserData = "Select * From User AS usr Where usr.Login = '@PARAM'";
+
+	private $title;
+	private $ptft;
+	private $vacationDays;
+	private $released;
+	private $first;     Varchar(255) NOT NULL,
+	private Last       Varchar(255) NOT NULL,
+	private Login      Varchar(255) NOT NULL UNIQUE,
+	private $hash       Varchar(255) NOT NULL,
+	private $salt
+	private $auth       Varchar(1024) NOT NULL,
+	private $time       DATE,
+	
 	public function User($login) {
 		$conn = DB::getNewConnection ();
 		$success = $conn->query ( str_replace ( "@PARAM", $login, self::$getUserData ) );
@@ -22,9 +35,23 @@ class User {
 /**/
 			if ($results === false)
 				die ( "faailed to get user, errno: " .$conn->errno ." error: " . "'" . $conn->error . "'" . mysqli_errno());
+
 			$arr = $results->fetch_array ();
 			$userData = $arr [0];
+
+			$this->type = $userData[1];
+			$this->ptft = (bool)$userData[2];
+			$this->vacation = (bool)$userData[3];
+				
 	/*		*/
+	}
+	
+	public function correctPassword($password) {
+	  return $this->hash === self::hashPassword($this->salt,$password);
+	}
+	
+	private static function hashPassword($salt,$password) {
+	  return ourhashingfunction($salt . $password);
 	}
 }
 
