8e94072dac906eb168cb1b04fef0ebfc89b5c251
Caleb <caleb@uwm.edu>
2014-03-19 22:21:02 -0500

    Updated home.php and contacts.php

--- ColdCalendars/home.php
+++ ColdCalendars/home.php
@@ -40,20 +40,6 @@ ini_set('display_errors', 3);
     <input type="reset" value="Cancel"/>
     </form>
     <script language="javascript">
-    function check(form)/*function to check userid & password*/
-    {
-    /*the following code checkes whether the entered userid and password are matching*/
-/*
-    if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd")
-    {
-    window.open('target.html')//opens the target page while Id & password matches
-    }
-    else
-    {
-    alert("Error Password or Username")//displays error message
-    }
-  */
-    }
     </script>
     </body>
     </html>
