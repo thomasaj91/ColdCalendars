55ba18d08198c507d096cdb9a806971102226fce
awashburn awashburn <awashburn1web@hotmail.com>
2014-03-25 08:12:27 -0500

    updated contacts.php to include main.js

--- ColdCalendars/contacts.php
+++ ColdCalendars/contacts.php
@@ -5,7 +5,8 @@
   <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
   <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
   <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-  <script src="contacts.js"></script> 
+  <script src="js/main.js"></script> 
+  <script src="js/contacts.js"></script> 
 </head>
 <body>
 
