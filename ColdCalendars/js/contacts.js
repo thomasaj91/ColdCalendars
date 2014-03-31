a98e0eaf42e06846d6809eeec47740ca8abbfb3c
cthosten <acethos@gmail.com>
2014-03-31 18:13:48 -0500

    Update contacts.js

--- ColdCalendars/js/contacts.js
+++ ColdCalendars/js/contacts.js
@@ -6,12 +6,77 @@ function loadContactsPage() {
 	  
 	  $("#Contact_List").accordion();
 	  $("#Current_User_Info").accordion();
+	  $("#Current_User_Info > div > ul").sortable();
 	  $("#Create_User").click(function() {
  	  		$( "#Create_User_Dialog" ).dialog("open");
       });
 	  
+	  $("#Logout").click(function () {
+		  window.location.href = "home.php";
+	  });
+	  
 	  $("#Delete_User").click(function() {
 	  		$( "#Delete_User_Dialog" ).dialog("open");
+      });
+	  
+	  $("#Add_Phone_Button").one('click', function() {
+		  var newPhone = $('<input>').insertAfter('#Current_User_Info > div > .phoneList').attr('id','New_Number');
+		  $('<button>').insertAfter(newPhone).text('Submit').attr('id','Submit_Phone');
+      });
+	  
+	  $(document).on('click', '#Submit_Phone', function(){
+		  var phoneObject = new Object();
+		  
+		  phoneObject.requestType = "AddPhone";
+		  phoneObject.phone		  = $('#New_Number').val();
+		  
+		  var retVal = $.ajax({
+				 url: "rest.php",
+				 data: "json="+JSON.stringify(phoneObject),
+				 dataType: "json",
+				 async: false
+		  });
+		  
+		 var obj = jQuery.parseJSON(retVal.responseText);
+		 if(obj === null) {
+			alert('unexpected server error');
+		 }
+		 else {
+			 if(obj['phone']===0)
+				 alert('Invalid phone number. Please try again.');
+			 else
+				 location.reload();
+		 }
+      });
+	  
+	  $("#Add_Email_Button").one('click', function() {
+		  var newEmail = $('<input>').insertAfter('#Current_User_Info > div > .emailList').attr('id','New_Email');
+		  $('<button>').insertAfter(newEmail).text('Submit').attr('id','Submit_Email');
+      });
+	  
+	  $(document).on('click', '#Submit_Email', function(){
+		  var emailObject = new Object();
+		  
+		  emailObject.requestType = "AddEmail";
+		  emailObject.email		  = $('#New_Email').val();
+		  
+		  var retVal = $.ajax({
+				 url: "rest.php",
+				 data: "json="+JSON.stringify(emailObject),
+				 dataType: "json",
+				 async: false
+		  });
+		  
+		 var obj = jQuery.parseJSON(retVal.responseText);
+		 if(obj === null) {
+			alert('unexpected server error');
+		 }
+		 else {
+			 if(obj['email']===0)
+				 alert('Invalid email address. Please try again.');
+			 else
+				 location.reload();
+		 }
       });
 	  
 	  $("#Create_User_Dialog").dialog({
@@ -125,6 +190,38 @@ function loadUser()
 				async: false
 				});
 		var list = jQuery.parseJSON(retVal.responseText); 
+		
+		//*********************************************************
+		var days  = ['SUN','MON','TUES','WED','THURS','FRI','SAT'];
+		var elem1  = $('#Schedule').empty();
+		var table = $('<table>').appendTo(elem1);
+		
+		var element = '<tr><td></td>';
+		for(var i in days)
+		{
+			element += '<td>';
+			element += days[i];
+			element += '</td>';
+		}
+		
+		element+='</tr>';
+		table.append(element);
+		
+		for(var e in list){
+			element = '<tr><td>';
+			var info1   = getInfoByLogin(list[e]);
+			element += info1.firstName + ' ' + info1.lastName + '</td>';
+			for(var i in days)
+			{
+				element += '<td>';
+				element += 'X';
+				element += '</td>';
+			}
+			element += '</tr>';
+			table.append(element);			
+		}
+		
+		//*********************************************************
 
 		var elem = $('#Contact_List').empty();
 		for(var e in list){
@@ -141,6 +238,12 @@ function loadUser()
 		$('#'+parseCookie().login+'_h3').remove().appendTo($('#Current_User_Info'));
 		$('#'+parseCookie().login+'_div').remove().appendTo($('#Current_User_Info'));
 		
+		var addPhoneButton = $('<button>').text('Add Phone Number').attr('id','Add_Phone_Button');
+		$('#Current_User_Info > div > .phoneList').prepend(addPhoneButton);
+		
+		var addEmailButton = $('<button>').text('Add Email Address').attr('id','Add_Email_Button');
+		$('#Current_User_Info > div > .emailList').prepend(addEmailButton);
+		
 		$("#Contact_List").accordion();
   }
   
@@ -149,8 +252,8 @@ function loadUser()
 	  var div = $('<div>').appendTo(elem).attr('id',login+'_div');
 	  $('<p>').appendTo(div).text('Title: ' + info.title)
 	  $('<p>').appendTo(div).text('Work Status: '+ (info.workStatus ? 'FT' : 'PT'));
-	  var li1 = $('<ul>').appendTo(div);
-	  var li2 = $('<ul>').appendTo(div);
+	  var li1 = $('<ul>').appendTo(div).attr('class','phoneList');
+	  var li2 = $('<ul>').appendTo(div).attr('class','emailList');
 	  for(var e in phones)
 		  $('<li>').appendTo(li1).text(phones[e]);
 	  for(var a in emails)
