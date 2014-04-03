f2f6d0ecccb6690e3a7750c07c4a0ab02d933044
thomasaj91 <thomasaj@uwm.edu>
2014-04-03 13:43:02 -0500

    Created autocomplete
    This is just a test file to show how the autocomplete works with the new rest and user functions. Populate the DB with users, and give this page a whirl.

---
+++ ColdCalendars/autocomplete.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>jQuery UI Autocomplete - Default functionality</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+  <script src="/js/schedule.js"></script>
+
+</head>
+<body>
+ 
+<div class="ui-widget">
+  <label for="tags">Tags: </label>
+  <input id="tags">
+</div>
+ 
+ 
+</body>
+</html>
