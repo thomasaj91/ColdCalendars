c81848a5651cdc065f25b67fff1803f6001afd5d
cthosten <acethos@gmail.com>
2014-04-13 12:18:59 -0500

    Added navbar/links
    Need to add logout functionality to the logout option on navbar. Eventually need to move navbar to centralized location.

--- ColdCalendars/schedule.php
+++ ColdCalendars/schedule.php
@@ -25,10 +25,63 @@
 		width: 900px;
 		margin: 0 auto;
 		}
-
+		
+	.center { margin : 0 auto; text-align: center; }
+	 
+	.queueItem { list-style-type: none; }
+	 
+	#navbar ul { 
+	          margin: 0; 
+	          padding: 5px; 
+	          list-style-type: none; 
+	          text-align: center; 
+	          background-color: #000; 
+	          } 
+	 
+	#navbar ul li {  
+	          display: inline; 
+	          } 
+	  
+	#navbar ul li a { 
+	          text-decoration: none; 
+	          padding: .2em 1em; 
+	          color: #fff; 
+	          background-color: #000; 
+	          } 
+	 
+	#navbar ul li a:hover { 
+	          color: #000; 
+	          background-color: #fff; 
+	          } 
+	#footer  { 
+	          font-size: 10px; 
+	          }
+	#container {
+	          width: 500px;
+	          height: 500px;
+	          background-color: #FDA;
+	          margin: 0 auto;
+	          overflow: hidden;
+	}
 </style>
 </head>
 <body>
+	<div id="wrapper" class="center">
+	    <header>
+		  <div id="navbar"> 
+		    <ul> 
+		        <li><a href="contacts.php">Contacts</a></li> 
+		        <li><a href="#">Requests</a></li> 
+		        <li><a href="schedule.php">Schedule</a></li> 
+		        <li><a href="#">Reports</a></li> 
+		        <li><a href="#">Log Out</a></li> 
+		    </ul> 
+		  </div> 
+		  <br>
+	   </header>
+	
+	</div>
+	
 	<div id='Shift_Options' title='Shift Options' style='display:none'>
 		<table>
 			<tr>
