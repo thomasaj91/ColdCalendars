78adcffff2671e171e295917039eaa50ec6f8291
thomasaj91 <thomasaj@uwm.edu>
2014-04-14 18:57:32 -0500

    Navbar logout now logs you out/removed logout button

--- ColdCalendars/contacts.php
+++ ColdCalendars/contacts.php
@@ -59,17 +59,17 @@ assertValidUserPageAccess();
 		  <div id="navbar"> 
 		    <ul> 
 		        <li><a href="contacts.php">Contacts</a></li> 
-		        <li><a href="#">Requests</a></li> 
+		        <li><a href="managerView_queue.php">Requests</a></li> 
 		        <li><a href="schedule.php">Schedule</a></li> 
 		        <li><a href="#">Reports</a></li> 
-		        <li><a href="#">Log Out</a></li> 
-		    </ul> 
+        		<li><a id='Logout' href="#" onclick="logUserOut();return false;">Log Out</a></li>
+		        </ul> 
 		  </div> 
 		  <br>
 	   </header>
 	
 	</div>
-<button id='Logout'>Logout</button>
+<!--  <button id='Logout'>Logout</button>-->
 <div id='Schedule'></div>
 <div id="Current_User_Info">
 </div>
