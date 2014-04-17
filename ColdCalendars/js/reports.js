bdac135e275c57acb4c9ab76faf9e43107dacdb8
thomasaj91 <thomasaj@uwm.edu>
2014-04-16 20:04:09 -0500

    updated with file creation functionality
    generates the correct csv file

--- ColdCalendars/js/reports.js
+++ ColdCalendars/js/reports.js
@@ -4,20 +4,22 @@ $(document).ready(function(){
 	$('#Report_End_Date').datepicker();
 	
 	$('#Generate_Report_Button').click(function() {
-			alert($('#Report_Start_Date').val() + ' ' + $('#Report_End_Date').val());
-			var A = [['n','sqrt(n)']];  // initialize array of rows with header row as 1st item
-			for(var j=1;j<10;++j){ A.push([j, Math.sqrt(j)]) }
-			var csvRows = [];
-			for(var i=0,l=A.length; i<l; ++i){
-			    csvRows.push(A[i].join(','));   // unquoted CSV row
-			}
-			var csvString = csvRows.join("\n");
-
-			var a = document.createElement('a');
-			a.href     = 'data:attachment/csv,' + csvString;
-			a.target   = '_blank';
-			a.download = 'myFile.csv';
-			document.body.appendChild(a);
-			a.click();
+		var startTime = $.datepicker.formatDate("yy-mm-dd", new Date($('#Report_Start_Date').val()));
+		var endTime = $.datepicker.formatDate("yy-mm-dd", new Date($('#Report_End_Date').val()));
+		
+		var reportObject = new Object();
+		reportObject.requestType = 'ReportExport';
+		reportObject.start = startTime;
+		reportObject.end = endTime;
+		var obj = ajaxGetJSON(reportObject);
+		
+		//alert(obj);
+		
+		var a = document.createElement('a');
+		a.href     = 'data:attachment/csv,' + encodeURIComponent(obj);
+		a.target   = '_blank';
+		a.download = 'myFile.csv';
+		document.body.appendChild(a);
+		a.click();
     }); 
 });
