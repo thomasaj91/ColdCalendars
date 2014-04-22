64ea1152278949b1f8e806eb709ba092653bf88f
cthosten <acethos@gmail.com>
2014-04-21 22:54:05 -0500

    Centralized navigation bar

---
+++ ColdCalendars/navbar.php
@@ -0,0 +1,16 @@
+<?php 
+?>
+<div id="wrapper" class="center">
+	<header>
+	 	<div id="navbar"> 
+	 		<ul> 
+				<li><a href="contacts.php">Contacts</a></li> 
+		    	<li><a href="managerView_queue.php">Requests</a></li> 
+		    	<li><a href="schedule.php">Schedule</a></li> 
+		    	<li><a href="#">Reports</a></li> 
+            	<li><a id='Logout' href="#" onclick="logUserOut();return false;">Log Out</a></li>
+        	</ul> 
+	 	</div> 
+		<br>
+   </header>
+</div>
