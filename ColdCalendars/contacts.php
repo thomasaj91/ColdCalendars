942bb3e2f5a5d0b06ec68f7ae1d7619c3bc7c6cf
cthosten <acethos@gmail.com>
2014-04-21 22:55:27 -0500

    Removed navbar code/included navbar.php

--- ColdCalendars/contacts.php
+++ ColdCalendars/contacts.php
@@ -18,21 +18,9 @@ assertValidUserPageAccess();
   <script src='../js/jquery.timepicker.min.js'></script>
 </head>
 <body>
-	<div id="wrapper" class="center">
-	    <header>
-		  <div id="navbar"> 
-		    <ul> 
-		        <li><a href="contacts.php">Contacts</a></li> 
-		        <li><a href="managerView_queue.php">Requests</a></li> 
-		        <li><a href="schedule.php">Schedule</a></li> 
-		        <li><a href="#">Reports</a></li> 
-        		<li><a id='Logout' href="#" onclick="logUserOut();return false;">Log Out</a></li>
-		        </ul> 
-		  </div> 
-		  <br>
-	   </header>
-	
-	</div>
+
+<?php include 'navbar.php'; ?>
+
 <!--  <button id='Logout'>Logout</button>-->
 <div id='Schedule'></div>
 <div id="Current_User_Info">
