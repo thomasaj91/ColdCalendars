96b2a43f8e4fbe5e6616b35eaa28746a1b89cdb0
thomasaj91 <thomasaj@uwm.edu>
2014-04-22 20:50:31 -0500

    updated getQueue to work for requests

--- ColdCalendars/js/queue.js
+++ ColdCalendars/js/queue.js
@@ -7,6 +7,7 @@ function getQueue() {
     queueObject.endTime = dateObjectToDateString(new Date("01/01/2099"));
      var list = ajaxGetJSON(queueObject);
      
+     /*
      for(var e in list) {
        var li  = $('<li>').appendTo('#Display_Queue').addClass('queueItem');
        var div = $('<div>').appendTo(li)
@@ -17,13 +18,25 @@ function getQueue() {
        $('<button>').addClass('acceptQueueItem').appendTo(div).text('+');
        $('<button>').addClass('rejectQueueItem').appendTo(div).text('-');
      }
+	*/
+     for(var e in list) {
+         var li  = $('<li>').appendTo('#Display_Queue').addClass('queueItem');
+         
+         var div = $('<div>').appendTo(li)
+                   .text(list[e]['login'] + " is requesting off from " + list[e]['startTime'] + " to " + list[e]['endTime'])
+                   .attr('data-login',list[e]['login'])
+                   .attr('data-startTime',list[e]['startTime'])
+                   .attr('data-endTime',list[e]['endTime']);
+         $('<button>').addClass('acceptQueueItem').appendTo(div).text('+');
+         $('<button>').addClass('rejectQueueItem').appendTo(div).text('-');
+       }
 }
 
 $(document).ready(function(){
 	setUserType();
 	hideManagerListOptions();
 	getQueue();
-	
+
     $('.acceptQueueItem').click(function(){
     	var acceptObj = new Object();
     	acceptObj.requestType = 'DecideSwap';
