434fe884e31f91c08d8c448788520c8c905255ac
cthosten <acethos@gmail.com>
2014-04-24 10:54:23 -0500

    Updated approvals link/added activity log

--- ColdCalendars/navbar.php
+++ ColdCalendars/navbar.php
@@ -4,12 +4,13 @@
 	<header>
 	 	<div id="navbar"> 
 	 		<ul id='Navbar_Options'> 
-			<li><a href="contacts.php">Contacts</a></li>
-			<li><a href="schedule.php">Schedule</a></li>
-			<li><a href="requests.php">Requests</a></li>
-			<li><a href='managerView_queue.php'>Approvals</a></li>  
-		    	<li><a href="reports.php">Reports</a></li> 
-            		<li><a id='Logout' href="#" onclick="logUserOut();return false;">Log Out</a></li>
+				<li><a href="contacts.php">Contacts</a></li>
+				<li><a href="schedule.php">Schedule</a></li>
+				<li><a href="requests.php">Requests</a></li>
+				<li><a href='managerQueue.php'>Approvals</a></li>  
+		    		<li><a href="reports.php">Reports</a></li> 
+		    		<li><a href='activityLog.php'>Activity Log</a>
+            			<li><a id='Logout' href="#" onclick="logUserOut();return false;">Log Out</a></li>
         		</ul> 
 	 	</div> 
 		<br>
