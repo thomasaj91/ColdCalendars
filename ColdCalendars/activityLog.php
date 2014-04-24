6a0c5c159e7e2bfb53893440143ecfa1aafa31c0
thomasaj91 <thomasaj@uwm.edu>
2014-04-23 22:06:38 -0500

    Create activityLog.php

---
+++ ColdCalendars/activityLog.php
@@ -0,0 +1,30 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Manager view</title>
+  <link rel="stylesheet" href="../css/coldcalendar.css" />
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+  <script src='../js/main.js'></script>
+  <script src="../js/activity.js"></script> 
+  </head> 
+  <body>
+
+  <?php include 'navbar.php'; ?>
+
+  <div class="center">
+    <hr size="3" color ="black">
+     <h1>Queue</h1>
+    <ul id="Display_Queue">
+    </ul>
+  </div>
+  <br>
+  <br>
+  <footer id="footer" class="center">
+  <p>Copyright Â© 2014 Cold Calendars</p>
+  </footer>
+</body>
+</html>
