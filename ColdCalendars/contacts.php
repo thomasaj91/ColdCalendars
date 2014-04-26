457a807ad7cf724509a39cc9a7bd6298e50c615d
thomasaj91 <thomasaj@uwm.edu>
2014-04-25 22:53:40 -0500

    Update contacts.php

--- ColdCalendars/contacts.php
+++ ColdCalendars/contacts.php
@@ -7,7 +7,7 @@ assertValidUserPageAccess();
 <html>
 <head>
   <meta charset="utf-8" />
-  <title>Employee View</title>
+  <title>Contacts</title>
   <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
   <link rel="stylesheet" href="../css/jquery.timepicker.css" />
   <link rel="stylesheet" href="../css/coldcalendar.css" />
